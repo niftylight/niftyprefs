6ec3c2ce18e0951a990131016b426a92b0d6dedf
daniel <daniel@localhost>
2014-04-25 13:27:13 +0200

    updated year in copyright statements to 2014

--- src/obj.h
+++ src/obj.h
@@ -1,6 +1,6 @@
 /*
  * libniftyprefs - lightweight modelless preferences management library
- * Copyright (C) 2006-2013 Daniel Hiepler <daniel@niftylight.de>
+ * Copyright (C) 2006-2014 Daniel Hiepler <daniel@niftylight.de>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
